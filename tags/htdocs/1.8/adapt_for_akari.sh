7fda211ec56a83b254c20e8dea31ae950749a5e2
jnguyen <jnguyen@7f79d943-42d6-424c-99fa-15cabed57ed8>
2011-05-15 09:30:07 +0000

    htdocs: adapt for AKARI

---
+++ tags/htdocs/1.8/adapt_for_akari.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# example usage:
+#   ./adapt_for_akari.sh index.html.en chapter-1.html.en *.html.en ...
+#   find -iname '*.html.en' -exec ./adapt_for_akari.sh '{}' \;
+
+for i in $@; do
+	sed -i -e 's/<title>TOMOYO Linux 1.8.x :/<title>AKARI :/g' ${i}
+	sed -i -e 's/tomoyotitle.png/akarititle.png/g' ${i}
+	sed -i -e 's/akarititle.png" width="320"/akarititle.png" width="174"/g' ${i}
+	sed -i -e '/tomoyo-changelogs/d' ${i}
+	sed -i -e '/tomoyo-download/d' ${i}
+done
+
+# The following replacement results in some unwanted changes so is not included:
+#   sed -i -e 's/TOMOYO Linux/AKARI/g' ${i}
